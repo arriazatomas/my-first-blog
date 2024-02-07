ae579fdfa33d1970abfd3982b9e3459fc0f2b214
arriazatomas <arriazatomas@correo.ugr.es>
2024-02-07 17:25:46 +0100

    Añadido vistas para crear y editar

--- etienda/urls.py
+++ etienda/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.productos, name='productos'),
     path('producto/<int:pk>/', views.producto_detallado, name='producto_detallado'),
+    path('producto/nuevo/', views.producto_nuevo, name='producto_nuevo'),
+    path('producto/<int:pk>/editar/', views.producto_editar, name='producto_editar'),
 ]
--- etienda/views.py
+++ etienda/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Producto
+from .form import ProductoForm
 
 # Create your views here.
 
@@ -10,4 +11,29 @@ def productos(request):
 def producto_detallado(request, pk):
     producto = Producto.objects.get(pk=pk)
     return render(request, 'etienda/producto_detallado.html', {'producto': producto})
+
+def producto_nuevo(request):
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            producto = form.save(commit=False)
+            producto.author = request.user
+            producto.save()
+            return redirect('producto_detallado', pk=producto.pk)
+    else:
+        form = ProductoForm()
+    return render(request, 'etienda/producto_nuevo.html', {'form': form})
+
+def producto_editar(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == "POST":
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            producto = form.save(commit=False)
+            producto.author = request.user
+            producto.save()
+            return redirect('producto_detallado', pk=producto.pk)
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'etienda/producto_editar.html', {'form': form})
 
