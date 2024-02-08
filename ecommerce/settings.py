464dd109fad1062ce955243c5ea8a8ca44244d75
arriazatomas <arriazatomas@correo.ugr.es>
2024-02-09 00:36:28 +0100

    Cambie algun HTML y agrego CSS.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,11 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'etienda/static')),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 MEIDA_URL = '/media/'
